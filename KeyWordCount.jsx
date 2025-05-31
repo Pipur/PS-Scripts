ff188fb0c37060a4d4be152e7eea55febc6e6f04
Peter Söderberg <peter@carreau.se>
2025-05-31 20:35:28 +0200

    Format documents

--- KeyWordCount.jsx
+++ KeyWordCount.jsx
@@ -1,16 +1,20 @@
-﻿KeyWordCount = function(event){  
-   if ((event.type == 'hover') && (event.object instanceof Thumbnail)) {
-           var Thumb  = new Thumbnail(event.object.spec);
-            var md = Thumb.synchronousMetadata;
-                md.namespace = "http://ns.adobe.com/photoshop/1.0/"; 
-                if(md.Keywords.length >0){
-            Thumb.core.itemContent.tooltip = "Keyword Count = " +md.Keywords.length;   
-            }
-      return { handled:false};
-  }
+﻿KeyWordCount = function(event) {
+    if ((event.type == 'hover') && (event.object instanceof Thumbnail)) {
+        var Thumb = new Thumbnail(event.object.spec);
+        var md = Thumb.synchronousMetadata;
+        md.namespace = "http://ns.adobe.com/photoshop/1.0/";
+        if (md.Keywords.length > 0) {
+            Thumb.core.itemContent.tooltip = "Keyword Count = " + md.Keywords.length;
+        }
+        return {
+            handled: false
+        };
+    }
 }
-app.eventHandlers.push( { handler: KeyWordCount } );
+app.eventHandlers.push({
+    handler: KeyWordCount
+});
 var prefs = app.preferences;
-try{
-if(!prefs.ShowTooltips) prefs.ShowTooltips=true;
-}catch(e){}
+try {
+    if (!prefs.ShowTooltips) prefs.ShowTooltips = true;
+} catch (e) {}
